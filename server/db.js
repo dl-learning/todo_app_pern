e539fcb3c46e2d74855b7772cf99df7e21a75f1c
Dark Dragon <ideapadlenovogaming.3@gmail.com>
2023-11-25 16:27:50 +0530

    configuring render psql_db

--- server/db.js
+++ server/db.js
@@ -1,11 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  port: 5432,
-  password: "postgres",
-  database: "test_db",
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const dotenv = require("dotenv");
-// dotenv.config();
+const dotenv = require("dotenv");
+dotenv.config();
 
 const pool = require("./db");
 
